141b475b20261063fda6ff7c77324ed7d6a4f077
syketbb <syket@algosolver.com>
2022-12-24 20:25:12 +0600

    theme setup

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { colors } from './utils/theme';
 
 export default function App() {
+  const [loadedFont] = useFonts({
+    InterRegular: require('./assets/fonts/Montserrat-Regular.ttf'),
+    InterMedium: require('./assets/fonts/Montserrat-Medium.ttf'),
+    InterSemibold: require('./assets/fonts/Montserrat-SemiBold.ttf'),
+    InterBold: require('./assets/fonts/Montserrat-Bold.ttf'),
+  });
+
+  if (!loadedFont) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ backgroundColor: colors.yellow }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ utils/theme.js
@@ -0,0 +1,15 @@
+export const colors = {
+  white: '#F9F9FB',
+  darkGray: '#313234',
+  lightGray: '#CDCDCD',
+  yellow: '#F5CA48',
+  darkOrange: '#F26C68',
+  lightOrange: '#E4723C',
+};
+
+export const fonts = {
+  regular: 'InterRegular',
+  medium: 'InterMedium',
+  semibold: 'InterSemibold',
+  bold: 'InterBold',
+};
